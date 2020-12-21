2b7cd0fe90f8fe96dce786ef3b4ca6ac6a70dc35
veselysps <veselym@spstrutnov.cz>
2020-12-22 00:21:16 +0100

    Add cors

--- listenmoe/__init__.py
+++ listenmoe/__init__.py
@@ -2,11 +2,15 @@ from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route
 from listenmoe import ws
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
 async def homepage(request):
     return JSONResponse({'song': ws.song, "artists": ws.artist})
 
-
+middleware = [
+    Middleware(CORSMiddleware, allow_origins=['*'])
+]
 app = Starlette(debug=True, routes=[
     Route('/', homepage),
-], on_startup=[ws.start_socket_thread])
+], on_startup=[ws.start_socket_thread], middleware=middleware)
